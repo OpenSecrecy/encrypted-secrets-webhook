2a5bb10ed6e64c3472fedd9907486fd9e4b7ca3b
shubhindia <shubhindia123@gmail.com>
2024-04-16 17:48:07 +0530

    initial webhook implementation
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

---
+++ webhook/webhook.go
@@ -0,0 +1,55 @@
+package webhook
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+type InitContainerAdder struct {
+	Client client.Client
+
+	log logr.Logger
+}
+
+//+kubebuilder:webhook:path=/mutate,mutating=true,failurePolicy=fail,groups=core,resources=pods,verbs=create,versions=v1,name=opensecrecy.io,admissionReviewVersions={v1,v1beta1},sideEffects=None
+
+func (i *InitContainerAdder) Handle(ctx context.Context, req admission.Request) admission.Response {
+	i.log = log.FromContext(ctx).WithValues("EncryptedSecret Webhook", req.Name)
+
+	var pod *corev1.Pod
+
+	if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+		i.log.Info("cannot unmarshal object")
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if pod.Annotations == nil {
+		pod.Annotations = map[string]string{}
+	}
+
+	pod.Spec.InitContainers = append(pod.Spec.InitContainers, corev1.Container{
+		Name:  "init-container",
+		Image: "busybox",
+		Command: []string{
+			"echo",
+			"Hello, World!",
+		},
+	})
+
+	marshaledPod, err := json.Marshal(pod)
+
+	if err != nil {
+		i.log.Info("cannot marshal object")
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
